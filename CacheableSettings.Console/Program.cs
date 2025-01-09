6d9abf9dabf4ae1af3df1d67fa6f3040990b03a7
iranasp.com <iranasp.com@gmail.com>
2025-01-09 12:22:07 +0330

    Add a console application to test package

---
+++ CacheableSettings.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using CacheableSettings.Library;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CacheableSettings.ConsoleApp
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            var cacheService = new ServiceCollection()
+                .AddSingleton<ISettingStore, SettingStore>()
+                .AddMemoryCache()
+                .BuildServiceProvider();
+
+            var service = cacheService.GetService<ISettingStore>();
+
+            var settings = new Dictionary<string, string>
+            {
+                {"item1","Hi"},
+                {"item2", "20"}
+            };
+
+            service.TTL = 120;
+            service.Store(settings);
+
+            var value = await service.GetOrCreate_Async("item1", () => "");
+
+            Console.WriteLine(value);
+            Console.ReadLine();
+
+        }
+    }
+}
