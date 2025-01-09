9d1d579e931a4270193930b0e768d4854e2c66a5
iranasp.com <iranasp.com@gmail.com>
2025-01-09 11:49:16 +0330

    Add project files.

---
+++ CacheableSettings.Library/ISettingStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CacheableSettings.Library
+{
+    public interface ISettingStore
+    {
+        /// <summary>
+        /// Time to live (seconds)
+        /// <para>Default value is 60 seconds</para>
+        /// </summary>
+        public int TTL { get; set; }
+
+        /// <summary>
+        /// Setting collection
+        /// </summary>
+        public Dictionary<string, string>? SettingItems { get; set; }
+
+        /// <summary>
+        /// Get stored item in memory or create it
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="createItem"></param>
+        /// <returns></returns>
+        Task<string?> GetOrCreate(string key, Func<string> createItem);
+
+        /// <summary>
+        /// Store all setting items in memory
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        void Store(Dictionary<string, string> items);
+    }
+}
---
+++ CacheableSettings.Library/SettingStore.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CacheableSettings.Library
+{
+    /// <summary>
+    /// Main class to manage setting items in memory
+    /// </summary>
+    public class SettingStore : ISettingStore
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public int TTL { get; set; }
+        public Dictionary<string, string>? SettingItems { get; set; }
+
+        public SettingStore(IMemoryCache memoryCache)
+        {
+            this._memoryCache = memoryCache;
+            TTL = 60;
+        }
+
+        public virtual Task<string?> GetOrCreate(string key, Func<string> createItem)
+        {
+            if (!_memoryCache.TryGetValue(key, out string? cachedValue))
+            {
+                cachedValue = createItem();
+
+                var option = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(TTL));
+
+                _memoryCache.Set(key, cachedValue, option);
+            }
+
+            return Task.FromResult(cachedValue);
+        }
+
+        public void Store(Dictionary<string, string> items)
+        {
+            foreach (var item in items)
+            {
+                _memoryCache.Remove(item.Key);
+                _memoryCache.Set(item.Key, item.Value);
+            }
+        }
+    }
+}
