8c9b1093f8b245f900a0aee92e368eba061dd572
iranasp.com <iranasp.com@gmail.com>
2025-02-06 17:30:35 +0330

    Add description to methods

--- CacheableSettings.Library/SettingStore.cs
+++ CacheableSettings.Library/SettingStore.cs
@@ -17,6 +17,13 @@ namespace CacheableSettings.Library
             this._memoryCache = memoryCache;
         }
 
+        /// <summary>
+        /// Get stored item in memory or create it
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="createItem">Your desired function</param>
+        /// <param name="ttl">Time to live in seconds - 0 means unlimited</param>
+        /// <returns></returns>
         public virtual Task<string?> GetOrCreate_Async(string key, Func<string> createItem, int ttl = 0)
         {
             if (!_memoryCache.TryGetValue(key, out string? cachedValue))
@@ -29,22 +36,31 @@ namespace CacheableSettings.Library
                 }
                 else
                 {
-                    var option = new MemoryCacheEntryOptions()
-                        .SetAbsoluteExpiration(TimeSpan.FromSeconds(ttl));
-                    _memoryCache.Set(key, cachedValue, option);
+                    _memoryCache.Set(key, cachedValue, new MemoryCacheEntryOptions()
+                        .SetAbsoluteExpiration(TimeSpan.FromSeconds(ttl)));
                 }
             }
 
             return Task.FromResult(cachedValue);
         }
 
-        public Task<string?> Get_Async(string key)
+        /// <summary>
+        /// Get an item from cache
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public virtual Task<string?> Get_Async(string key)
         {
             _memoryCache.TryGetValue(key, out string? value);
             return Task.FromResult(value);
         }
 
-        public void Store(Dictionary<string, string> items)
+        /// <summary>
+        /// Store all setting items in memory
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public virtual void Store(Dictionary<string, string> items)
         {
             foreach (var item in items)
             {
